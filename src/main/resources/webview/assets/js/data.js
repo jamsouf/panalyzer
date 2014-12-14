73e7764e0bb86ee9816af1173364897cf9d5cf2d
Jamil Soufan <jamil@soufan.de>
2014-12-14 20:09:46 +0000

    extending webview

--- src/main/resources/webview/assets/js/data.js
+++ src/main/resources/webview/assets/js/data.js
@@ -5,9 +5,12 @@
 var Data = {
     project: '/var/www/langr.io/',
     analyzed: 'Thu, 15 Dec 2014 14:05',
+    sizeTotal: '14.7 MB',
     folders: 32284,
     files: 958147,
     linesOfCode: 121852364,
-    top5FileExt: ['PHP', 'JAVA', 'CSS', 'XML', 'ZIP'],
-    top5FileExtCount: [1245, 813, 234, 75, 23]
+    topFileExt: ['PHP', 'JAVA', 'CSS', 'XML', 'ZIP'],
+    topFileExtCount: [1245, 813, 234, 75, 23],
+    topLocFolders: ['texts/', 'model/', 'js/', 'sql/', 'html/'],
+    topLocFoldersCount: [156324, 95421, 32154, 8652, 1835],
 };
--- src/main/resources/webview/assets/js/run.js
+++ src/main/resources/webview/assets/js/run.js
@@ -7,14 +7,13 @@
     init();
     
     var title = 'panalyzer / ',
-        delay = 100,
         app = $.sammy(function() {
     
         this.get('#/overview', function() {
             document.title = title + 'overview';
-            $("#overview").show(delay);
-            $("#folders").hide(delay);
-            $("#files").hide(delay);
+            $("#overview").show();
+            $("#folders").hide();
+            $("#files").hide();
             $("#clk-overview").addClass('active');
             $("#clk-folders").removeClass('active');
             $("#clk-files").removeClass('active');
@@ -22,9 +21,9 @@
         
         this.get('#/folders', function() {
             document.title = title + 'folders';
-            $("#overview").hide(delay);
-            $("#folders").show(delay);
-            $("#files").hide(delay);
+            $("#overview").hide();
+            $("#folders").show();
+            $("#files").hide();
             $("#clk-overview").removeClass('active');
             $("#clk-folders").addClass('active');
             $("#clk-files").removeClass('active');
@@ -32,9 +31,9 @@
         
         this.get('#/files', function() {
             document.title = title + 'files';
-            $("#overview").hide(delay);
-            $("#folders").hide(delay);
-            $("#files").show(delay);
+            $("#overview").hide();
+            $("#folders").hide();
+            $("#files").show();
             $("#clk-overview").removeClass('active');
             $("#clk-folders").removeClass('active');
             $("#clk-files").addClass('active');
@@ -53,11 +52,14 @@
 function init () {
     write('dt.project', Data.project);
     write('dt.analyzed', Data.analyzed);
+    write('dt.sizeTotal', Data.sizeTotal);
     write('dt.folders', nbr(Data.folders));
     write('dt.files', nbr(Data.files));
     write('dt.loc', nbr(Data.linesOfCode));
-    
     generateTopFileExtChart();
+    
+    write('dt.folders2', nbr(Data.folders));
+    generateTopLocFolders();
 }
 
 /**
@@ -103,17 +105,63 @@ function generateTopFileExtChart() {
                 showInLegend: false
             }
         },
+        credits: {
+            enabled: false
+        },
         series: [{
             type: 'pie',
             name: 'File types',
             data: [
-                [Data.top5FileExt[0], Data.top5FileExtCount[0]],
-                [Data.top5FileExt[1], Data.top5FileExtCount[1]],
-                [Data.top5FileExt[2], Data.top5FileExtCount[2]],
-                [Data.top5FileExt[3], Data.top5FileExtCount[3]],
-                [Data.top5FileExt[4], Data.top5FileExtCount[4]]
+                [Data.topFileExt[0], Data.topFileExtCount[0]],
+                [Data.topFileExt[1], Data.topFileExtCount[1]],
+                [Data.topFileExt[2], Data.topFileExtCount[2]],
+                [Data.topFileExt[3], Data.topFileExtCount[3]],
+                [Data.topFileExt[4], Data.topFileExtCount[4]]
             ]
         }],
         colors: ['#354F5D', '#34B39E', '#E1584D', '#EEA19B', '#BCBCBC']
+    });
+}
+
+/**
+ * Create the chart for the
+ * most line of code in folders
+ */
+function generateTopLocFolders() {
+    $('#topLocFolders').highcharts({
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: null
+        },
+        xAxis: {
+            categories: Data.topLocFolders,
+            title: {
+                text: null
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: null
+            }
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.y}</b></b>'
+        },
+        plotOptions: {
+            bar: {
+                showInLegend: false
+            }
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            name: 'Lines of code',
+            data: Data.topLocFoldersCount
+        }],
+        colors: ['#3E4A50']
     });
 }
