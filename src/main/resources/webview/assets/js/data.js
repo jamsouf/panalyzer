9f8aea7ada817c01733fd912829d4cc1f0f87907
Jamil Soufan <jamil@soufan.de>
2014-12-15 20:56:43 +0000

    new page files and other stuff

--- src/main/resources/webview/assets/js/data.js
+++ src/main/resources/webview/assets/js/data.js
@@ -3,14 +3,92 @@
  * from the analyzing
  */
 var Data = {
+    
+    // Analyzed folder
     project: '/var/www/langr.io/',
+    
+    // Time of analyzing
     analyzed: 'Thu, 15 Dec 2014 14:05',
-    sizeTotal: '14.7 MB',
-    folders: 32284,
-    files: 958147,
-    linesOfCode: 121852364,
-    topFileExt: ['PHP', 'JAVA', 'CSS', 'XML', 'ZIP'],
-    topFileExtCount: [1245, 813, 234, 75, 23],
-    topLocFolders: ['texts/', 'model/', 'js/', 'sql/', 'html/'],
-    topLocFoldersCount: [156324, 95421, 32154, 8652, 1835],
+    
+    // Total size of the analyzed folder
+    totalSize: 15414067.2,
+    
+    // Total count of folders
+    totalFolders: 32284,
+    
+    // Total count of files
+    totalFiles: 958147,
+    
+    // Total count of lines of code
+    totalLoc: 121852364,
+    
+    // Top X files per file extension
+    topFilesExt: {
+        0: { ident: 'PHP', value: 1245 },
+        1: { ident: 'JAVA', value: 813 },
+        2: { ident: 'CSS', value: 234 },
+        3: { ident: 'XML', value: 75 },
+        4: { ident: 'ZIP', value: 23 }
+    },
+    
+    // Top X folders per files
+    topFoldersFiles: {
+        0: { ident: 'langr.io/src/Langr/Model/', value: 321 },
+        1: { ident: 'langr.io/src/Langr/db/sql/', value: 145 },
+        2: { ident: 'langr.io/src/Langr/texts/de/', value: 31 },
+        3: { ident: 'langr.io/src/Langr/resources/img/', value: 14 },
+        4: { ident: 'langr.io/src/Langr/View/', value: 9 }
+    },
+    
+    // Top X folders per size
+    topFoldersSize: {
+        0: { ident: 'img/', value: 8808038.364 },
+        1: { ident: 'resources/', value: 3355443.214 },
+        2: { ident: 'js/', value: 26521.65415}
+    },
+    
+    // Top X folders per lines of code
+    topFoldersLoc: {
+        0: { ident: 'texts/', value: 156324 },
+        1: { ident: 'model/', value: 95421 },
+        2: { ident: 'js/', value: 32154 },
+        3: { ident: 'sql/', value: 8652 },
+        4: { ident: 'html/', value: 1835 }
+    },
+    
+    // Top X files per file size
+    topFilesSize: {
+        0: { ident: 'de/jamilsoufan/model/entity/person/wrapper/Model.php', value: 4852.475 },
+        1: { ident: 'Data.php', value: 2951.325 },
+        2: { ident: 'Values.java', value: 1563.74475 },
+        3: { ident: 'style.css', value: 1123.2 },
+        4: { ident: 'de/jamilsoufan/model/entity/person/wrapper/javascript/run.js', value: 914.2 }
+    },
+    
+    // Top X files per lines of code
+    topFilesLoc: {
+        0: { ident: 'UpdateSqlValues.sql', value: 3214 },
+        1: { ident: 'DataMiningWrapper.java', value: 2142 },
+        2: { ident: 'styles.css', value: 1502 },
+        3: { ident: 'german.txt', value: 605 },
+        4: { ident: 'backup_values.php.txt', value: 102 }
+    },
+    
+    // Top X file extensions per lines of code
+    topExtLoc: {
+        0: { ident: 'JAVA', value: 32458 },
+        1: { ident: 'JS', value: 10452 },
+        2: { ident: 'PHP', value: 3241 },
+        3: { ident: 'TXT', value: 752 },
+        4: { ident: 'XML', value: 521 }
+    },
+    
+    // Top X files per last modification
+    topFilesLastMod: {
+        0: { ident: '/www/data/langr.io/src/Langr/Model/Renderer/Data.php', owner: 'www-data', date: '2014/11/12', time: '23:11' },
+        1: { ident: '/www/data/langr.io/src/Langr/db/sql/create.sql', owner: 'Administrator', date: '2014/07/04', time: '17:44' },
+        2: { ident: '/www/data/langr.io/src/Langr/texts/de/messages.txt', owner: 'www-data', date: '2014/05/27', time: '02:25' },
+        3: { ident: '/www/data/langr.io/src/Langr/texts/de/errors.txt', owner: 'root', date: '2014/04/16', time: '22:14' },
+        4: { ident: '/www/data/langr.io/src/Langr/resources/static/images/dynamicTex.img', owner: 'www-data', date: '2014/04/15', time: '15:02' }
+    }
 };
--- src/main/resources/webview/assets/js/run.js
+++ src/main/resources/webview/assets/js/run.js
@@ -50,16 +50,95 @@
  * at startup with values
  */
 function init () {
-    write('dt.project', Data.project);
-    write('dt.analyzed', Data.analyzed);
-    write('dt.sizeTotal', Data.sizeTotal);
-    write('dt.folders', nbr(Data.folders));
-    write('dt.files', nbr(Data.files));
-    write('dt.loc', nbr(Data.linesOfCode));
-    generateTopFileExtChart();
-    
-    write('dt.folders2', nbr(Data.folders));
-    generateTopLocFolders();
+    initOverview();
+    initFolders();
+    initFiles();
+}
+
+/**
+ * Create and insert the
+ * data for the overview-site
+ */
+function initOverview() {
+    var max = 85;
+    write('d.project', cut(Data.project, max));
+    write('d.analyzed', Data.analyzed);
+    write('d.totalSize', size(Data.totalSize));
+    /*************************************************************/
+    write('d.totalFolders', nbr(Data.totalFolders));
+    /*************************************************************/
+    write('d.totalFiles', nbr(Data.totalFiles));
+    /*************************************************************/
+    write('d.totalLoc', nbr(Data.totalLoc));
+    /*************************************************************/
+    generateTopFilesExtChart();
+}
+
+/**
+ * Create and insert the
+ * data for the folders-site
+ */
+function initFolders() {
+    var files = ' files';
+    write('tff-value-1', Data.topFoldersFiles[0].value+files);
+    write('tff-value-2', Data.topFoldersFiles[1].value+files);
+    write('tff-value-3', Data.topFoldersFiles[2].value+files);
+    write('tff-value-4', Data.topFoldersFiles[3].value+files);
+    write('tff-value-5', Data.topFoldersFiles[4].value+files);
+    var max = 50;
+    write('tff-ident-1', cut(Data.topFoldersFiles[0].ident, max));
+    write('tff-ident-2', cut(Data.topFoldersFiles[1].ident, max));
+    write('tff-ident-3', cut(Data.topFoldersFiles[2].ident, max));
+    write('tff-ident-4', cut(Data.topFoldersFiles[3].ident, max));
+    write('tff-ident-5', cut(Data.topFoldersFiles[4].ident, max));
+    /*************************************************************/
+    write('d.totalFolders-2', nbr(Data.totalFolders));
+    /*************************************************************/
+    write('tfs-value-1', size(Data.topFoldersSize[0].value));
+    write('tfs-value-2', size(Data.topFoldersSize[1].value));
+    write('tfs-value-3', size(Data.topFoldersSize[2].value));
+    max = 18;
+    write('tfs-ident-1', cut(Data.topFoldersSize[0].ident, max));
+    write('tfs-ident-2', cut(Data.topFoldersSize[1].ident, max));
+    write('tfs-ident-3', cut(Data.topFoldersSize[2].ident, max));
+    /*************************************************************/
+    generateTopFoldersLocChart();
+}
+
+/**
+ * Create and insert the
+ * data for the files-site
+ */
+function initFiles() {
+    write('d.totalFiles-2', nbr(Data.totalFiles));
+    /*************************************************************/
+    generateTopFilesSizeChart();
+    /*************************************************************/
+    generateTopFilesLocChart();
+    /*************************************************************/
+    generateTopExtLocChart();
+    /*************************************************************/
+    write('tflm-date-1', Data.topFilesLastMod[0].date);
+    write('tflm-date-2', Data.topFilesLastMod[1].date);
+    write('tflm-date-3', Data.topFilesLastMod[2].date);
+    write('tflm-date-4', Data.topFilesLastMod[3].date);
+    write('tflm-date-5', Data.topFilesLastMod[4].date);
+    write('tflm-time-1', Data.topFilesLastMod[0].time);
+    write('tflm-time-2', Data.topFilesLastMod[1].time);
+    write('tflm-time-3', Data.topFilesLastMod[2].time);
+    write('tflm-time-4', Data.topFilesLastMod[3].time);
+    write('tflm-time-5', Data.topFilesLastMod[4].time);
+    write('tflm-owner-1', Data.topFilesLastMod[0].owner);
+    write('tflm-owner-2', Data.topFilesLastMod[1].owner);
+    write('tflm-owner-3', Data.topFilesLastMod[2].owner);
+    write('tflm-owner-4', Data.topFilesLastMod[3].owner);
+    write('tflm-owner-5', Data.topFilesLastMod[4].owner);
+    var max = 68;
+    write('tflm-ident-1', file(cut(Data.topFilesLastMod[0].ident, max)));
+    write('tflm-ident-2', file(cut(Data.topFilesLastMod[1].ident, max)));
+    write('tflm-ident-3', file(cut(Data.topFilesLastMod[2].ident, max)));
+    write('tflm-ident-4', file(cut(Data.topFilesLastMod[3].ident, max)));
+    write('tflm-ident-5', file(cut(Data.topFilesLastMod[4].ident, max)));
 }
 
 /**
@@ -79,11 +158,59 @@ function nbr(n) {
 }
 
 /**
+ * Convert the byte value
+ * into better readable format
+ */
+function size(bytes) {
+    if(bytes == 0) return '0 Byte';
+    var k = 1000;
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    var i = Math.floor(Math.log(bytes) / Math.log(k));
+    return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
+}
+
+/**
+ * Cut the first parts of
+ * a string if it's too long
+ */
+function cut(v, max) {
+    if (v.length > max) {
+        v = '[...]' + v.slice(-max);
+    }
+    return v;
+}
+
+/**
+ * Highlight the file in
+ * a path string
+ */
+function file(str) {
+    var n = str.lastIndexOf('/');
+    var path = str.substring(0, n + 1);
+    var file = str.substring(n + 1);
+    return '<span class="path-start">'+path+'</span><span class="path-end">'+file+'</span>';
+}
+
+/**
+ * Highlight the last folder
+ * in a path string
+ */
+function folder(str) {
+    if (str.slice(-1) == '/') {
+        str = str.substring(0, str.length - 1);
+    }
+    var n = str.lastIndexOf('/');
+    var path = str.substring(0, n + 1);
+    var folder = str.substring(n + 1);
+    return '<span class="path-start">'+path+'</span><span class="path-end">'+folder+'/</span>';
+}
+
+/**
  * Create the chart for the
  * allocation of file types
  */
-function generateTopFileExtChart() {
-    $('#topFileExt').highcharts({
+function generateTopFilesExtChart() {
+    $('#topFilesExt').highcharts({
         chart: {
             plotBackgroundColor: null,
             plotBorderWidth: null,
@@ -93,7 +220,7 @@ function generateTopFileExtChart() {
             text: null
         },
         tooltip: {
-            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b> ({point.y})'
         },
         plotOptions: {
             pie: {
@@ -112,11 +239,11 @@ function generateTopFileExtChart() {
             type: 'pie',
             name: 'File types',
             data: [
-                [Data.topFileExt[0], Data.topFileExtCount[0]],
-                [Data.topFileExt[1], Data.topFileExtCount[1]],
-                [Data.topFileExt[2], Data.topFileExtCount[2]],
-                [Data.topFileExt[3], Data.topFileExtCount[3]],
-                [Data.topFileExt[4], Data.topFileExtCount[4]]
+                [Data.topFilesExt[0].ident, Data.topFilesExt[0].value],
+                [Data.topFilesExt[1].ident, Data.topFilesExt[1].value],
+                [Data.topFilesExt[2].ident, Data.topFilesExt[2].value],
+                [Data.topFilesExt[3].ident, Data.topFilesExt[3].value],
+                [Data.topFilesExt[4].ident, Data.topFilesExt[4].value]
             ]
         }],
         colors: ['#354F5D', '#34B39E', '#E1584D', '#EEA19B', '#BCBCBC']
@@ -127,8 +254,8 @@ function generateTopFileExtChart() {
  * Create the chart for the
  * most line of code in folders
  */
-function generateTopLocFolders() {
-    $('#topLocFolders').highcharts({
+function generateTopFoldersLocChart() {
+    $('#topFoldersLoc').highcharts({
         chart: {
             type: 'bar'
         },
@@ -136,7 +263,13 @@ function generateTopLocFolders() {
             text: null
         },
         xAxis: {
-            categories: Data.topLocFolders,
+            categories: [
+                Data.topFoldersLoc[0].ident,
+                Data.topFoldersLoc[1].ident,
+                Data.topFoldersLoc[2].ident,
+                Data.topFoldersLoc[3].ident,
+                Data.topFoldersLoc[4].ident
+            ],
             title: {
                 text: null
             }
@@ -160,8 +293,176 @@ function generateTopLocFolders() {
         },
         series: [{
             name: 'Lines of code',
-            data: Data.topLocFoldersCount
+            data: [
+                Data.topFoldersLoc[0].value,
+                Data.topFoldersLoc[1].value,
+                Data.topFoldersLoc[2].value,
+                Data.topFoldersLoc[3].value,
+                Data.topFoldersLoc[4].value
+            ]
         }],
         colors: ['#3E4A50']
+    });
+}
+
+/**
+ * Create the chart for the
+ * biggest files by size
+ */
+function generateTopFilesSizeChart () {
+    $('#topFilesSize').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: null
+        },
+        xAxis: {
+            categories: [
+                Data.topFilesSize[0].ident,
+                Data.topFilesSize[1].ident,
+                Data.topFilesSize[2].ident,
+                Data.topFilesSize[3].ident,
+                Data.topFilesSize[4].ident
+            ],
+            labels: {
+                enabled: false
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: null
+            }
+        },
+        tooltip: {
+            formatter: function() {
+                return '<span style="font-size:10px">' + this.x + '</span><br>' + 
+                        this.series.name + ': <b>' + size(this.point.y) + '</b></b>';
+            }
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0,
+                showInLegend: false
+            }
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            name: 'Size',
+            data: [
+                Data.topFilesSize[0].value,
+                Data.topFilesSize[1].value,
+                Data.topFilesSize[2].value,
+                Data.topFilesSize[3].value,
+                Data.topFilesSize[4].value
+            ]
+        }],
+        colors: ['#34B39E']
+    });
+}
+
+/**
+ * Create the chart for the
+ * biggest files by lines of code
+ */
+function generateTopFilesLocChart () {
+    $('#topFilesLoc').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: null
+        },
+        xAxis: {
+            categories: [
+                Data.topFilesLoc[0].ident,
+                Data.topFilesLoc[1].ident,
+                Data.topFilesLoc[2].ident,
+                Data.topFilesLoc[3].ident,
+                Data.topFilesLoc[4].ident
+            ],
+            labels: {
+                enabled: false
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: null
+            }
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.y}</b></b>'
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0,
+                showInLegend: false
+            }
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            name: 'Lines of code',
+            data: [
+                Data.topFilesLoc[0].value,
+                Data.topFilesLoc[1].value,
+                Data.topFilesLoc[2].value,
+                Data.topFilesLoc[3].value,
+                Data.topFilesLoc[4].value
+            ]
+        }],
+        colors: ['#E1584D']
+    });
+}
+
+/**
+ * Create the chart for the
+ * allocation of lines of code by file types
+ */
+function generateTopExtLocChart() {
+    $('#topExtLoc').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        },
+        title: {
+            text: null
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.y}</b> ({point.percentage:.1f}%)'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true
+                },
+                showInLegend: false
+            }
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            type: 'pie',
+            name: 'Lines of code',
+            data: [
+                [Data.topExtLoc[0].ident, Data.topExtLoc[0].value],
+                [Data.topExtLoc[1].ident, Data.topExtLoc[1].value],
+                [Data.topExtLoc[2].ident, Data.topExtLoc[2].value],
+                [Data.topExtLoc[3].ident, Data.topExtLoc[3].value],
+                [Data.topExtLoc[4].ident, Data.topExtLoc[4].value]
+            ]
+        }],
+        colors: ['#354F5D', '#34B39E', '#E1584D', '#EEA19B', '#BCBCBC']
     });
 }
